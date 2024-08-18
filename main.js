e455a0a2dbbc8584ee72d40c732c5db2d8c48df6
rohyoonseo <ajnaval67@gmail.com>
2024-08-18 22:46:40 +0800

    some changes
    burgerburgerburger

--- main.js
+++ main.js
@@ -5,3 +5,23 @@ menuElement.addEventListener('click', (ev) => {
     console.log(menuDiv);
     menuDiv.classList.toggle('toRight');
 })
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
